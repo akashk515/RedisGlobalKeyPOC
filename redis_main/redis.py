07cae7d4b4d37fb6e5f5e819cbb8fde89e73c14c
Akash Kumar <akash@grexit.com>
2024-04-09 17:32:44 +0530

    update

--- redis_main/redis.py
+++ redis_main/redis.py
@@ -10,7 +10,7 @@ redis_host = config['REDIS_MAIN']['HOST']
 redis_port = config['REDIS_MAIN']['PORT']
 db_number = config['REDIS_MAIN']['DB']
 
-log.log('connection to redis syncer stats db', redis_host, redis_port, db_number)
+log.info('connection to redis syncer stats db', redis_host, redis_port, db_number)
 
 POOL = redis.ConnectionPool(host=redis_host, port=redis_port, db=db_number, decode_responses=True)
 reds = redis.Redis(connection_pool=POOL)
